dc0b4e9e446e70e04f482f86b8dd9c8ff6b114be
hee <424252111@qq.com>
2016-04-25 10:13:35 +0800

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ app.config["MONGODB_SETTINGS"] = {'DB': "heeblog",'PORT' : 27019} # set DB name
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 db = MongoEngine(app)
 def register_blueprints(app):
-    from hee.views import posts
+    from app.views import posts
     app.register_blueprint(posts)
 
 register_blueprints(app)
