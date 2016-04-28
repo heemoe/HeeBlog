12df46a53971879d407652569a1846e2eba9ad4f
hee <424252111@qq.com>
2016-04-28 17:58:38 +0800

    a few RESTful APIs

--- __init__.py
+++ __init__.py
@@ -1,11 +1,26 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {'DB': "heeblog", 'PORT': 27019}  # set DB name
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 db = MongoEngine(app)
 
+api = Api(app)
+class PostAPI(Resource):
+    # def __init__(self):
+    #     self.reqparse = reqparse.RequestParser()
+    #     self.reqparse.add_argument('slug', type = str, required = True, help = 'No post', location = 'json')
+    #     self.reqparse.add_argument('description', type = str, default = "", location = 'json')
+    #     super(PostAPI, self).__init__()
+
+    def get(self):
+        return { 'task': 'test' }
+    # def post(self, id):
+    #     pass
+
+# api.add_resource(PostAPI, '/Posts/<app: int>', endpoint='test')
 
 def register_blueprints(app):
     from views import posts
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask_script import Manager, Server
 from __init__ import app
 from livereload import Server
